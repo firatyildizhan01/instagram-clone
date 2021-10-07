d1e80db28582c0a3ac18f215d2a65ed06e70b42a
FIRAT-YILDIZHAN <hanyildizfirat@hotmail.com>
2021-10-07 07:59:09 +0300

    Commit

---
+++ app/src/main/java/com/example/kotlininstagram/view/MainActivity.kt
@@ -0,0 +1,70 @@
+package com.example.kotlininstagram.view
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.kotlininstagram.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        auth = FirebaseAuth.getInstance()
+
+        val curruntUser = auth.currentUser
+
+        if (curruntUser != null) {
+            val intent = Intent(this, FeedActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    fun signInClicked(view: View) {
+
+        val email = binding.emailText.text.toString()
+        val password = binding.passwordText.text.toString()
+
+        if (email.equals("") || password.equals("")) {
+            Toast.makeText(this, "Enter email and password!", Toast.LENGTH_LONG).show()
+        } else {
+            auth.signInWithEmailAndPassword(email, password).addOnSuccessListener {
+                val intent = Intent(this@MainActivity, FeedActivity::class.java)
+                startActivity(intent)
+                finish()
+            }.addOnFailureListener {
+                Toast.makeText(this@MainActivity, it.localizedMessage, Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    fun signUpClicked(view: View) {
+
+        val email = binding.emailText.text.toString()
+        val password = binding.passwordText.text.toString()
+
+        if (email.equals("") || password.equals("")) {
+            Toast.makeText(this, "Enter email and password!", Toast.LENGTH_LONG).show()
+        } else {
+            auth.createUserWithEmailAndPassword(email, password).addOnSuccessListener {
+                val intent = Intent(this@MainActivity, FeedActivity::class.java)
+                startActivity(intent)
+                finish()
+            }.addOnFailureListener {
+                Toast.makeText(this@MainActivity, it.localizedMessage, Toast.LENGTH_LONG).show()
+            }
+        }
+
+
+    }
+}
